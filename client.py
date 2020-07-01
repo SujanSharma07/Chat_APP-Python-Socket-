44e13ff90086570fe727443be7b7063fcfd9c5d1
SujanSharma07 <sujansharma202@gmail.com>
2020-07-01 21:53:15 +0545

    GUI Added

--- client.py
+++ client.py
@@ -1,40 +1,123 @@
-import socket
-import pickle
-import time
-import threading
+
+from tkinter import Tk, Frame, Scrollbar, Label, END, Entry, Text, VERTICAL, Button, messagebox #Tkinter Python Module for GUI  
+import socket #Sockets for network connection
+import threading # for multiple proccess 
 
 
-def receive():
-	while True:
-		time1 = time.asctime( time.localtime(time.time()) )
-		received = pickle.loads(c.recv(1024))
-		print("message from my friend ("+str(time1)+")"+ str(received))
 
-def send():
-	while True:
-		name = input("Message from myside: ")
-		name = pickle.dumps(name)
-		c.send(name)
+class GUI:
+    client_socket = None
+    last_received_message = None
+    
+    def __init__(self, master):
+        self.root = master
+        self.chat_transcript_area = None
+        self.name_widget = None
+        self.enter_text_widget = None
+        self.join_button = None
+        self.initialize_socket()
+        self.initialize_gui()
+        self.listen_for_incoming_messages_in_a_thread()
 
-#SOCK_STREAM corresponds to TCP and AF_INET corresponds to IPV4
-c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def initialize_socket(self):
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # initialazing socket with TCP and IPv4
+        remote_ip = '127.0.0.1' # IP address 
+        remote_port = 10319 #TCP port
+        self.client_socket.connect((remote_ip, remote_port)) #connect to the remote server
 
+    def initialize_gui(self): # GUI initializer
+        self.root.title("Socket Chat") 
+        self.root.resizable(0, 0)
+        self.display_chat_box()
+        self.display_name_section()
+        self.display_chat_entry_box()
+    
+    def listen_for_incoming_messages_in_a_thread(self):
+        thread = threading.Thread(target=self.receive_message_from_server, args=(self.client_socket,)) # Create a thread for the send and receive in same time 
+        thread.start()
+    #function to recieve msg
+    def receive_message_from_server(self, so):
+        while True:
+            buffer = so.recv(256)
+            if not buffer:
+                break
+            message = buffer.decode('utf-8')
+         
+            if "joined" in message:
+                user = message.split(":")[1]
+                message = user + " has joined"
+                self.chat_transcript_area.insert('end', message + '\n')
+                self.chat_transcript_area.yview(END)
+            else:
+                self.chat_transcript_area.insert('end', message + '\n')
+                self.chat_transcript_area.yview(END)
 
-# SO_ - socket option
-# SOL_ - socket option level
-# Sets REUSEADDR (as a socket option) to 1 on socket
-c.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        so.close()
 
-#local host ko thau ma ip address rakhni if different devices
-c.connect((socket.gethostname(),9919))
+    def display_name_section(self):
+        frame = Frame()
+        Label(frame, text='Enter your name:', font=("Helvetica", 16)).pack(side='left', padx=10)
+        self.name_widget = Entry(frame, width=50, borderwidth=2)
+        self.name_widget.pack(side='left', anchor='e')
+        self.join_button = Button(frame, text="Join", width=10, command=self.on_join).pack(side='left')
+        frame.pack(side='top', anchor='nw')
 
-t1 = threading.Thread(target = receive)
-t2 = threading.Thread(target = send )
+    def display_chat_box(self):
+        frame = Frame()
+        Label(frame, text='Chat Box:', font=("Serif", 12)).pack(side='top', anchor='w')
+        self.chat_transcript_area = Text(frame, width=60, height=10, font=("Serif", 12))
+        scrollbar = Scrollbar(frame, command=self.chat_transcript_area.yview, orient=VERTICAL)
+        self.chat_transcript_area.config(yscrollcommand=scrollbar.set)
+        self.chat_transcript_area.bind('<KeyPress>', lambda e: 'break')
+        self.chat_transcript_area.pack(side='left', padx=10)
+        scrollbar.pack(side='right', fill='y')
+        frame.pack(side='top')
 
-t1.start()
-t2.start()
+    def display_chat_entry_box(self):
+        frame = Frame()
+        Label(frame, text='Enter message:', font=("Serif", 12)).pack(side='top', anchor='w')
+        self.enter_text_widget = Text(frame, width=60, height=3, font=("Serif", 12))
+        self.enter_text_widget.pack(side='left', pady=15)
+        self.enter_text_widget.bind('<Return>', self.on_enter_key_pressed)
+        frame.pack(side='top')
 
-	
-	
-	
+    def on_join(self):
+        if len(self.name_widget.get()) == 0:
+            messagebox.showerror(
+                "Enter your name", "Enter your name to send a message")
+            return
+        self.name_widget.config(state='disabled')
+        self.client_socket.send(("joined:" + self.name_widget.get()).encode('utf-8'))
 
+    def on_enter_key_pressed(self, event):
+        if len(self.name_widget.get()) == 0:
+            messagebox.showerror("Enter your name", "Enter your name to send a message")
+            return
+        self.send_chat()
+        self.clear_text()
+
+    def clear_text(self):
+        self.enter_text_widget.delete(1.0, 'end')
+
+    def send_chat(self):
+        senders_name = self.name_widget.get().strip() + ": "
+        data = self.enter_text_widget.get(1.0, 'end').strip()
+        message = (senders_name + data).encode('utf-8')
+        self.chat_transcript_area.insert('end', message.decode('utf-8') + '\n')
+        self.chat_transcript_area.yview(END)
+        self.client_socket.send(message)
+        self.enter_text_widget.delete(1.0, 'end')
+        return 'break'
+
+    def on_close_window(self):
+        if messagebox.askokcancel("Quit", "Do you want to quit?"):
+            self.root.destroy()
+            self.client_socket.close()
+            exit(0)
+
+#the mail function 
+if __name__ == '__main__':
+    root = Tk()
+    gui = GUI(root)
+    root.protocol("WM_DELETE_WINDOW", gui.on_close_window)
+    root.mainloop()
--- server.py
+++ server.py
@@ -1,71 +1,58 @@
-import socket
-import pickle
-import time
+#imports
+import socket 
 import threading
 
-class client_socket():
-    def __init__(self,c,number):
-        self.number = number
-        self.number_t = (self.number+1)%2
-        self.client_from = c[self.number]['socket']
-        self.client_to = c[self.number_t]['socket']
 
-    def receive(self):
-        
+class ChatServer:
+    
+    clients_list = []
+
+    last_received_message = ""
+
+    def __init__(self):
+        self.server_socket = None
+        self.create_listening_server()
+    #listen for incoming connection
+    def create_listening_server(self):
+    
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #create a socket using TCP port and ipv4
+        local_ip = '127.0.0.1'
+        local_port = 10319
+        # this will allow you to immediately restart a TCP server
+        self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # this makes the server listen to requests coming from other computers on the network
+        self.server_socket.bind((local_ip, local_port))
+        print("Listening for incoming messages..")
+        self.server_socket.listen(5) #listen for incomming connections / max 5 clients
+        self.receive_messages_in_a_new_thread()
+    #fun to receive new msgs
+    def receive_messages(self, so):
         while True:
-           
-            self.message = pickle.loads(self.client_from.recv(1024))
-            self.sendto(self.client_to,self.message)
+            incoming_buffer = so.recv(256) #initialize the buffer
+            if not incoming_buffer:
+                break
+            self.last_received_message = incoming_buffer.decode('utf-8')
+            self.broadcast_to_all_clients(so)  # send to all clients
+        so.close()
+    #broadcast the message to all clients 
+    def broadcast_to_all_clients(self, senders_socket):
+        for client in self.clients_list:
+            socket, (ip, port) = client
+            if socket is not senders_socket:
+                socket.sendall(self.last_received_message.encode('utf-8'))
+
+    def receive_messages_in_a_new_thread(self):
+        while True:
+            client = so, (ip, port) = self.server_socket.accept()
+            self.add_to_clients_list(client)
+            print('Connected to ', ip, ':', str(port))
+            t = threading.Thread(target=self.receive_messages, args=(so,))
+            t.start()
+    #add a new client 
+    def add_to_clients_list(self, client):
+        if client not in self.clients_list:
+            self.clients_list.append(client)
 
 
-
-    def sendto(self,c,data):
-        self.data = pickle.dumps(data)
-        c.send(self.data)
-
-
-
-	
-
-#SOCK_STREAM corresponds to TCP and AF_INET corresponds to IPV4
-s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-# SO_ - socket option
-# SOL_ - socket option level
-# Sets REUSEADDR (as a socket option) to 1 on socket
-s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-print(" Server Socket Created")
-
-#local host indicate ip adddress and 9919 indixate the port
-s.bind((socket.gethostname(),9919))
-
-#this is to listen cleint, 3 indicate maximum number of client we will handeling
-s.listen(2)
-
-print("Waiting for the client request")
-#to accept the connectio n from client
-
-clients = []
-clients_info = {}
-clients_number = 0
-
-while True:
-    c, addr = s.accept()
-    clients_number+=1
-    print(f"client with c {c} address1 {addr}")
-    print(f"Total {clients_number} clients are connected now")
-    clients_info = {"socket":c,"addr":addr}
-    clients.append(clients_info)
-    if clients_number==2:
-        break
-
-print(f"all clients information is {clients}")
-
-client1 = client_socket(clients,0)
-client2 = client_socket(clients,1)
-t1 = threading.Thread(target = client1.receive)
-t2 = threading.Thread(target = client2.receive)
-t1.start()
-t2.start()
+if __name__ == "__main__":
+    ChatServer()
